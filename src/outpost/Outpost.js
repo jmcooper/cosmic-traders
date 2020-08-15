86633160e91c164eaef7a86dd6bb8210057ecddd
Jim Cooper <jim@cooperplanet.com>
2020-08-14 23:48:58 -0600

    Functional component

--- src/outpost/Outpost.js
+++ src/outpost/Outpost.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-class Outpost extends React.Component {
-  render() {
-    return <h1>Welcome to the outpost!</h1>
-  }
+function Outpost() {
+  return <h1>Welcome to the outpost!</h1>
 }
 
-  export default Outpost
+export default Outpost

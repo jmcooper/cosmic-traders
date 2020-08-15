3ad5290dfa10307978a833b3fa81634230c731e7
Jim Cooper <jim@cooperplanet.com>
2020-08-15 11:37:04 -0600

    Add images to outpost page

--- src/outpost/Outpost.js
+++ src/outpost/Outpost.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PowerCrystalsImage from '../images/power-crystals.png'
 
 function Outpost() {
   return (
@@ -6,7 +7,7 @@ function Outpost() {
       <h1 className="section-header">Outpost</h1>
       <ul className="products">
         <li className="product">
-          <img src="" alt="warp core"/>
+          <img src={ require('../images/warp-core.png') } alt="warp core"/>
           <h2 className="title">Warp Core</h2>
           <div className="priceLine">
             <h3 className="price">$29,995</h3>
@@ -14,7 +15,7 @@ function Outpost() {
           </div>
         </li>
         <li className="product">
-          <img src="" alt="Power Crystals"/>
+          <img src={ PowerCrystalsImage } alt="Power Crystals"/>
           <h2 className="title">Power Crystals</h2>
           <div className="priceLine">
             <h3 className="price">$1,995</h3>
